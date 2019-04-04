81c7b7aa03eafcddb40e24955d7d32e2fde29b47
MarioEstrada <maestradaz3@gmail.com>
2019-04-04 16:13:21 -0500

    feat: Modelo de tercero y movimiento

---
+++ src/Entity/Movimiento.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * MovimientoType
+ *
+ * @ORM\Table(name="movimiento")
+ * @ORM\Entity(repositoryClass="App\Repository\MovimientoRepository")
+ */
+class Movimiento
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $codigoMovimientoPk;
+
+    /**
+     * @ORM\Column(name="fecha", type="datetime", nullable=true)
+     */
+    private $fecha;
+
+    /**
+     * @ORM\Column(name="codigo_tercero_fk", type="integer", nullable=true)
+     */
+    private $codigoTerceroFk;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Tercero", inversedBy="movimientosTerceroRel")
+     * @ORM\JoinColumn(name="codigo_tercero_fk", referencedColumnName="codigo_tercero_pk")
+     */
+    private $terceroRel;
+
+    /**
+     * @return mixed
+     */
+    public function getCodigoMovimientoPk()
+    {
+        return $this->codigoMovimientoPk;
+    }
+
+    /**
+     * @param mixed $codigoMovimientoPk
+     */
+    public function setCodigoMovimientoPk($codigoMovimientoPk): void
+    {
+        $this->codigoMovimientoPk = $codigoMovimientoPk;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    /**
+     * @param mixed $fecha
+     */
+    public function setFecha($fecha): void
+    {
+        $this->fecha = $fecha;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCodigoTerceroFk()
+    {
+        return $this->codigoTerceroFk;
+    }
+
+    /**
+     * @param mixed $codigoTerceroFk
+     */
+    public function setCodigoTerceroFk($codigoTerceroFk): void
+    {
+        $this->codigoTerceroFk = $codigoTerceroFk;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTerceroRel()
+    {
+        return $this->terceroRel;
+    }
+
+    /**
+     * @param mixed $terceroRel
+     */
+    public function setTerceroRel($terceroRel): void
+    {
+        $this->terceroRel = $terceroRel;
+    }
+
+
+
+}
---
+++ src/Entity/Tercero.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TerceroType
+ *
+ * @ORM\Table(name="tercero")
+ * @ORM\Entity(repositoryClass="App\Repository\TerceroRepository")
+ */
+class Tercero
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $codigoTerceroPk;
+
+    /**
+     * @ORM\Column(name="nombre_corto", type="string", length=150, nullable=true)
+     */
+    private $nombreCorto;
+
+    /**
+     * @ORM\Column(name="cliente", type="boolean", nullable=true, options={"default" : false})
+     */
+    private $cliente = false;
+
+    /**
+     * @ORM\Column(name="proveedor", type="boolean", nullable=true, options={"default" : false})
+     */
+    private $proveedor = false;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Movimiento", mappedBy="terceroRel")
+     */
+    protected $movimientosTerceroRel;
+
+    /**
+     * @return mixed
+     */
+    public function getCodigoTerceroPk()
+    {
+        return $this->codigoTerceroPk;
+    }
+
+    /**
+     * @param mixed $codigoTerceroPk
+     */
+    public function setCodigoTerceroPk($codigoTerceroPk): void
+    {
+        $this->codigoTerceroPk = $codigoTerceroPk;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombreCorto()
+    {
+        return $this->nombreCorto;
+    }
+
+    /**
+     * @param mixed $nombreCorto
+     */
+    public function setNombreCorto($nombreCorto): void
+    {
+        $this->nombreCorto = $nombreCorto;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCliente()
+    {
+        return $this->cliente;
+    }
+
+    /**
+     * @param mixed $cliente
+     */
+    public function setCliente($cliente): void
+    {
+        $this->cliente = $cliente;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProveedor()
+    {
+        return $this->proveedor;
+    }
+
+    /**
+     * @param mixed $proveedor
+     */
+    public function setProveedor($proveedor): void
+    {
+        $this->proveedor = $proveedor;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMovimientosTerceroRel()
+    {
+        return $this->movimientosTerceroRel;
+    }
+
+    /**
+     * @param mixed $movimientosTerceroRel
+     */
+    public function setMovimientosTerceroRel($movimientosTerceroRel): void
+    {
+        $this->movimientosTerceroRel = $movimientosTerceroRel;
+    }
+
+
+
+}
