87815da650c66278a1649fa8ca4d9000abe9e6f7
sebas957 <baxtian957@hotmail.com>
2019-05-28 15:06:51 -0500

    Datafixture Entidad GEN_BANCO

--- src/Entity/General/GenBanco.php
+++ src/Entity/General/GenBanco.php
@@ -21,6 +21,26 @@ class GenBanco
     private $nombre;
 
     /**
+     * @ORM\Column(name="codigo_general", type="string", length=20, nullable=true)
+     */
+    private $codigoGeneral;
+
+    /**
+     * @ORM\Column(name="nit", type="string", length=10, nullable=true)
+     */
+    private $nit;
+
+    /**
+     * @ORM\Column(name="direccion", type="string", length=80, nullable=true)
+     */
+    private $direccion;
+
+    /**
+     * @ORM\Column(name="telefono", type="string", length=15, nullable=true)
+     */
+    private $telefono;
+
+    /**
      * @ORM\OneToMany(targetEntity="GenCuenta", mappedBy="bancoRel")
      */
     protected $cuentasBancoRel;
@@ -60,6 +80,86 @@ class GenBanco
     /**
      * @return mixed
      */
+    public function getNumeroDigitos()
+    {
+        return $this->numeroDigitos;
+    }
+
+    /**
+     * @param mixed $numeroDigitos
+     */
+    public function setNumeroDigitos($numeroDigitos): void
+    {
+        $this->numeroDigitos = $numeroDigitos;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCodigoGeneral()
+    {
+        return $this->codigoGeneral;
+    }
+
+    /**
+     * @param mixed $codigoGeneral
+     */
+    public function setCodigoGeneral($codigoGeneral): void
+    {
+        $this->codigoGeneral = $codigoGeneral;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNit()
+    {
+        return $this->nit;
+    }
+
+    /**
+     * @param mixed $nit
+     */
+    public function setNit($nit): void
+    {
+        $this->nit = $nit;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDireccion()
+    {
+        return $this->direccion;
+    }
+
+    /**
+     * @param mixed $direccion
+     */
+    public function setDireccion($direccion): void
+    {
+        $this->direccion = $direccion;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+    /**
+     * @param mixed $telefono
+     */
+    public function setTelefono($telefono): void
+    {
+        $this->telefono = $telefono;
+    }
+
+    /**
+     * @return mixed
+     */
     public function getCuentasBancoRel()
     {
         return $this->cuentasBancoRel;
@@ -72,5 +172,7 @@ class GenBanco
     {
         $this->cuentasBancoRel = $cuentasBancoRel;
     }
+
+
 
 }
