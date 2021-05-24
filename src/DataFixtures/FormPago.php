8203efef8b0ce0dbce758df785a8132f5a75cdc4
MarioA <maestradaz3@gmail.com>
2021-05-23 21:32:42 -0500

    Corregir datafixturas

--- src/DataFixtures/FormPago.php
+++ src/DataFixtures/FormPago.php
@@ -5,25 +5,24 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class GenFormPago extends Fixture
+class FormPago extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $arFormaPago = $manager->getRepository(\App\Entity\General\GenFormaPago::class)->find('CON');
+        $arFormaPago = $manager->getRepository(\App\Entity\FormaPago::class)->find('CON');
         if(!$arFormaPago){
             $arFormaPago = new \App\Entity\General\GenFormaPago();
             $arFormaPago->setCodigoFormaPagoPk('CON');
             $arFormaPago->setNombre('CONTADO');
             $manager->persist($arFormaPago);
         }
-        $arFormaPago = $manager->getRepository(\App\Entity\General\GenFormaPago::class)->find('CRE');
+        $arFormaPago = $manager->getRepository(\App\Entity\FormaPago::class)->find('CRE');
         if(!$arFormaPago){
-            $arFormaPago = new \App\Entity\General\GenFormaPago();
+            $arFormaPago = new \App\Entity\FormaPago();
             $arFormaPago->setCodigoFormaPagoPk('CRE');
             $arFormaPago->setNombre('CREDITO');
             $manager->persist($arFormaPago);
         }
-
         $manager->flush();
     }
 }
